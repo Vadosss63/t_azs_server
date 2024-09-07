f6d5ec79c4cb53d670fd52d69f172a3d3841ab8e
Vadosss63 <parusovvadim@mail.ru>
2024-09-07 12:52:41 +0300

    Add TestGetStationsHandler

--- internal/controllers/ya_controller/ya_controller_test.go
+++ internal/controllers/ya_controller/ya_controller_test.go
@@ -63,20 +63,55 @@ func TestGetPriceListHandler(t *testing.T) {
 	assert.JSONEq(t, expectedJSONresponse, rr.Body.String())
 }
 
-// func TestGetStationsHandler(t *testing.T) {
-// 	app := &application.App{}
-// 	controller := NewController(app)
+func TestGetStationsHandler(t *testing.T) {
+	ctrl := gomock.NewController(t)
+	defer ctrl.Finish()
 
-// 	req, err := http.NewRequest("GET", "/tanker/station?apikey=expected_api_key", nil)
-// 	assert.NoError(t, err)
+	mockYaAzsRepo := ya_azs.NewMockYaAzsRepository(ctrl)
+	mockAzsRepo := azs.NewMockAzsRepository(ctrl)
 
-// 	rr := httptest.NewRecorder()
-// 	router := httprouter.New()
-// 	router.GET("/tanker/station", controller.GetStationsHandler)
-// 	router.ServeHTTP(rr, req)
+	mocStation := ya_azs.Station{Id: "1", Enable: true, Location: ya_azs.Location{Lat: 11, Lon: 12}, Columns: map[int32]ya_azs.Column{1: {Fuels: []string{"a92", "a95"}}}}
+	mockYaAzsRepo.EXPECT().GetEnableAll(gomock.Any()).Return([]ya_azs.Station{mocStation}, nil)
 
-// 	assert.Equal(t, http.StatusOK, rr.Code)
-// }
+	mockAzsData := azs.AzsStatsData{
+		Id:                  1,
+		IdAzs:               1,
+		IdUser:              1,
+		IsAuthorized:        1,
+		CountColum:          2,
+		IsSecondPriceEnable: 1,
+		Time:                "2024-09-07T10:00:00Z",
+		Name:                "AZS Example",
+		Address:             "123 Main St",
+		Stats:               `{"azs_nodes":[{"averageTemperature":"0.00","commonLiters":"0.00","dailyLiters":"0.00","density":"0.00","fuelArrival":0,"fuelVolume":"0","fuelVolumePerc":"0.00","lockFuelValue":60,"price":4927,"priceCashless":5522,"typeFuel":"АИ-92"},{"averageTemperature":"0.00","commonLiters":"0.00","dailyLiters":"0.00","density":"0.00","fuelArrival":0,"fuelVolume":"0","fuelVolumePerc":"0.00","lockFuelValue":100,"price":5101,"priceCashless":5200,"typeFuel":"АИ-95"}],"main_info":{"commonCash":0,"commonCashless":0,"commonOnline":0,"dailyCash":0,"dailyCashless":0,"dailyOnline":0,"isBlock":false,"version":"1.0.2"}}`,
+	}
+
+	mockAzsRepo.EXPECT().Get(gomock.Any(), gomock.Any()).Return(mockAzsData, nil).AnyTimes()
+
+	mocRepo := repository.Repository{
+		YaAzsRepo: mockYaAzsRepo,
+		AzsRepo:   mockAzsRepo,
+	}
+
+	app := &application.App{
+		Repo: &mocRepo,
+	}
+
+	controller := NewController(app)
+
+	req, err := http.NewRequest("GET", "/tanker/station?apikey=expected_api_key", nil)
+	assert.NoError(t, err)
+
+	rr := httptest.NewRecorder()
+	router := httprouter.New()
+	router.GET("/tanker/station", controller.GetStationsHandler)
+	router.ServeHTTP(rr, req)
+
+	assert.Equal(t, http.StatusOK, rr.Code)
+
+	expectedJSONresponse := `[{"Id":"1","Enable":true,"Name":"AZS Example","Address":"123 Main St","Location":{"Lat":11,"Lon":12},"Columns":{"0":{"Fuels":["a92"]},"1":{"Fuels":["a92","a95","a95"]}}}]`
+	assert.JSONEq(t, expectedJSONresponse, rr.Body.String())
+}
 
 // func TestPingHandler(t *testing.T) {
 // 	app := &application.App{}
